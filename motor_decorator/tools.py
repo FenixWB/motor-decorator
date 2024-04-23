5a6beaf018b6be95a1f807a5fd4e6fc5a5809d51
Tim <timkin99@mail.ru>
2024-04-23 15:05:20 +0700

    feature: Updated error handling in MotorDecoratorTools
    Enhanced the error handling implementation in MotorDecoratorTools class by adding additional error case for BulkWriteError. A default instance of MotorDecoratorRetryParameters is also added as a base_retry_param. The error messages for exception handling have been updated for more clarity.

--- motor_decorator/tools.py
+++ motor_decorator/tools.py
@@ -3,7 +3,7 @@ import functools
 import logging
 from typing import Callable, Any
 
-from pymongo.errors import DuplicateKeyError
+from pymongo.errors import DuplicateKeyError, BulkWriteError
 
 from .objects import MotorDecoratorRetryParameters
 
@@ -11,6 +11,7 @@ from .objects import MotorDecoratorRetryParameters
 class MotorDecoratorTools:
     info_format: str = "[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s [%(filename)s/%(funcName)s:%(lineno)d]"
     LOGGING_LEVEL: str = "INFO"
+    base_retry_param: MotorDecoratorRetryParameters = MotorDecoratorRetryParameters()
 
     @staticmethod
     def retry(logger: logging.Logger, init_retries: int = 3, timeout: int = 1) -> Callable:
@@ -21,22 +22,37 @@ class MotorDecoratorTools:
                 retries = init_retries
                 delay = timeout
 
-                retry_param = kwargs.pop("retry_param", MotorDecoratorRetryParameters())
+                retry_param = kwargs.pop("retry_param", MotorDecoratorTools.base_retry_param)
 
                 while retries:
                     try:
                         return await func(*args, **kwargs)
                     except DuplicateKeyError as ex:
                         error_message = (
-                            f"Database connection error (retry={retries % 4}). "
+                            f"DuplicateKeyError (retry={retries % 4}). "
                             f"execution function: <{func.__name__}>, "
-                            f"exception class: <{ex.__class__.__name__}>, "
                             f"exception description: {ex}"
                         )
 
                         if retry_param.skip_duplicate_key_error_info is False:
                             logger.error(error_message)
                         return
+                    except BulkWriteError as ex:
+                        error_message = (
+                            f"BulkWriteError (retry={retries % 4}). "
+                            f"execution function: <{func.__name__}>, "
+                            f"exception description: {ex}"
+                        )
+                        if error_description := ex.details.get('writeErrors', []):
+                            if "E11000" in error_description[0].get("errmsg", ""):
+                                if retry_param.skip_duplicate_key_error_info is False:
+                                    logger.error(error_message)
+                                return
+
+                        logger.error(error_message)
+                        retries -= 1
+                        await asyncio.sleep(delay)
+                        delay *= 2
                     except Exception as ex:
                         error_message = (
                             f"Database connection error (retry={retries % 4}). "
